3956cc6fafc1d80d5f24bf6969d42708395e3e58
Dhiego <dhiegofs1@gmail.com>
2023-10-19 16:44:48 -0300

    Adicionada tela Home
    Adicionada tela Home

--- digitalmenu2/src/pages/Home/Home.jsx
+++ digitalmenu2/src/pages/Home/Home.jsx
@@ -1,19 +1,53 @@
 import { useNavigate } from 'react-router-dom'
+import './home.css';
 
-function Home(){
+import logoDM from '../../assets/image/logo_digitalmenu2.png';
+import fotoApresentacao from '../../assets/image/fotoApresentacao.jpg';
+
+function Home() {
 
     const navigate = useNavigate();
 
-    return(
+    return (
         <>
             <div>
-                <h1>Home</h1>
-                <button onClick={() => {
-                    localStorage.removeItem("token");
-                    navigate('/')
-                }}>Sair</button>
-                <button onClick={()=>navigate('/system')}>Área do Sistema</button>
-                <button onClick={()=>navigate('/client')}>Área do Cliente</button>
+
+                <header>
+                    <nav>
+                        <div className='home_cabecalho'>
+                            <img className='home_dmLogo' src={logoDM} alt="" />
+
+                            <button className="btn_sair_home" onClick={() => {
+                                localStorage.removeItem("token");
+                                navigate('/')
+                            }}>Sair</button>
+
+                        </div>
+                    </nav>
+                </header>
+
+
+                <main>
+                    <div className='home_apresentacao'>
+                        <div className='home_imagem'>
+                            <img className='fotoApresentacao' src={fotoApresentacao} alt="" />
+                            <p className='home-descricao'>DIGITAL MENU é um sistema de gerenciamento de pedidos de restaurantes...</p>
+
+                        </div>
+
+                        <div className='home_opcoes'>
+                            <img src="./images/foto.jpg" alt="" />
+                            <p className='home-entrar-descricao'>DigitalMenu, entre como administrador ou cliente:</p>
+                            
+                            <button onClick={() => navigate('/system')}>Área do Sistema</button>
+                            <button onClick={() => navigate('/client')}>Área do Cliente</button>
+
+                        </div>
+                    </div>
+                </main>
+
+
+
             </div>
         </>
     );
