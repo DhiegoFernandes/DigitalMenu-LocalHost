f0024b25153fdd424b7c67eb2b4720731ea305af
Guigite <guicunhaalves@gmail.com>
2023-10-19 12:28:05 -0300

    01

--- digitalmenu2/src/context/context.jsx
+++ digitalmenu2/src/context/context.jsx
@@ -1,6 +1,6 @@
 import { createContext} from "react";
 import { useNavigate } from "react-router-dom";
-import api from "../api";
+import api from '../api'
 import { createNotification } from '../util/toastify'
 
 // Criação do contexto
@@ -19,10 +19,9 @@ function MainProvider({ children }) {
         senha = senha.trim();
 
         try{
-            const { data } = await api.post('/login', {nome, senha})
-            localStorage.setItem("token", data.token)
+            const { data } = await api.post('/login', {nome, senha});
+            localStorage.setItem("token", data.token);
             api.defaults.headers.Authorization = `Bearer ${data.token}`
-            createNotification("Seja bem vindo!", 'success');
             navigate('/home')
         } catch (e) {
             createNotification("Erro no login", 'error');
